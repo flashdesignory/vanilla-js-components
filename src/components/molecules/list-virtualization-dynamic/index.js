f1fcab3f2c763dd2f7c82ee470760acf7ec5a4be
Thorsten Kober <tkober@gmail.com>
2022-10-26 23:21:11 -0700

    dynamic list

---
+++ src/components/molecules/list-virtualization-dynamic/index.js
@@ -0,0 +1,5 @@
+import { List } from './list.js';
+import { data } from './data.js';
+
+const ref = document.getElementById('app');
+new List({ ref, data, displayHeight: 240 }).render();
---
+++ src/components/molecules/list-virtualization-dynamic/item.js
@@ -0,0 +1,48 @@
+export class Item {
+  constructor({ data, yPosition }) {
+    this.data = data;
+    this.yPosition = yPosition;
+  }
+
+  render() {
+    this.containerElement = document.createElement('div');
+    this.containerElement.classList.add('item-container');
+    this.containerElement.style.transform = `translateY(${this.yPosition}px)`;
+
+    // setting height depending on type
+    if (this.data.type === 'text') {
+      this.containerElement.classList.add('text');
+    } else if (this.data.type === 'image') {
+      this.containerElement.classList.add('image');
+    }
+
+    const avatarElement = document.createElement('div');
+    avatarElement.classList.add('item-avatar');
+    this.containerElement.appendChild(avatarElement);
+
+    const contentElement = document.createElement('div');
+    contentElement.classList.add('item-content');
+    this.containerElement.appendChild(contentElement);
+
+    if (this.data.type === 'text') {
+      const descriptionElement = document.createElement('div');
+      descriptionElement.classList.add('item-description');
+      descriptionElement.textContent = this.data.content;
+      contentElement.appendChild(descriptionElement);
+    } else if (this.data.type === 'image') {
+      const imageElement = document.createElement('img');
+      imageElement.classList.add('item-image');
+      imageElement.src = this.data.source;
+      imageElement.width = this.data.width;
+      imageElement.height = this.data.height;
+      contentElement.appendChild(imageElement);
+    }
+
+    const metadataElement = document.createElement('div');
+    metadataElement.classList.add('item-metadata');
+    metadataElement.textContent = this.data.metadata;
+    contentElement.appendChild(metadataElement);
+
+    return this.containerElement;
+  }
+}
