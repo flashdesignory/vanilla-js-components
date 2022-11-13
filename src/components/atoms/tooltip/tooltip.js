349fda963c7f97572fa0c698ae80152e60d70fde
Thorsten Kober <tkober@gmail.com>
2022-11-13 13:44:04 -0800

    tooltip js

--- src/components/atoms/tooltip/tooltip.js
+++ src/components/atoms/tooltip/tooltip.js
@@ -13,6 +13,10 @@ export class Tooltip {
     };
 
     this.style = style;
+    this.timeout = undefined;
+
+    this.handleOnMouseOver = this.handleOnMouseOver.bind(this);
+    this.handleOnMouseOut = this.handleOnMouseOut.bind(this);
 
     this.container = document.createElement("div");
     this.container.classList.add("tooltip-container");
@@ -25,6 +29,9 @@ export class Tooltip {
 
     this.button = document.createElement("button");
     this.button.classList.add("button");
+    this.button.addEventListener("mouseover", this.handleOnMouseOver);
+    this.button.addEventListener("mouseleave", this.handleOnMouseOut);
+    this.button.addEventListener("mouseout", this.handleOnMouseOut);
     this.container.appendChild(this.button);
 
     this.tooltip = document.createElement("div");
@@ -41,6 +48,17 @@ export class Tooltip {
     if (label !== undefined) this.state.label = label;
     if (type !== undefined) this.state.type = type;
     if (content !== undefined) this.state.content = content;
+  }
+
+  handleOnMouseOver() {
+    clearTimeout(this.timeout);
+    this.tooltip.classList.add("show");
+    this.timeout = setTimeout(this.handleOnMouseOut, 2000);
+  }
+
+  handleOnMouseOut() {
+    clearTimeout(this.timeout);
+    this.tooltip.classList.remove("show");
   }
 
   render() {
