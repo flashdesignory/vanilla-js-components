14069dbcb1cf7516432ef5b525fdefdb1dbd419d
Thorsten Kober <tkober@gmail.com>
2022-11-15 16:19:43 -0800

    Add files via upload

--- src/assets/email.js
+++ src/assets/email.js
@@ -1,1 +1,2 @@
-export const EmailIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><title>email icon</title><path d="M12 12.713l-11.985-9.713h23.97l-11.985 9.713zm0 2.574l-12-9.725v15.438h24v-15.438l-12 9.725z"/></svg>`;
+export const EmailIcon = ({ width = "24", height = "24" }) =>
+  `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 24 24"><title>email icon</title><path d="M12 12.713l-11.985-9.713h23.97l-11.985 9.713zm0 2.574l-12-9.725v15.438h24v-15.438l-12 9.725z"/></svg>`;
