122f3a8963a1c6f378125019a0b2860f3b0c3630
Thorsten Kober <tkober@gmail.com>
2022-11-15 16:21:38 -0800

    Add files via upload

---
+++ src/components/organisms/image-carousel/image-carousel.js
@@ -0,0 +1,121 @@
+// import sheet from './image-carousel.css' assert { type: 'css' };
+// document.adoptedStyleSheets.push(sheet);
+import "./image-carousel.css";
+
+import { DisplayImage } from "../../molecules/display-image/display-image.js";
+import { Button } from "../../atoms/button/button.js";
+import { ArrowLeft } from "../../../assets/arrow-left.js";
+import { ArrowRight } from "../../../assets/arrow-right.js";
+
+export class ImageCarousel {
+  constructor({ data = [], itemHeight, itemWidth }) {
+    this.state = {
+      data: [], // {src: string }[]
+      itemHeight: undefined, // number
+      itemWidth: undefined, // number
+    };
+
+    // initial values
+    this.items = {};
+    this.slideIndex = 1;
+
+    this.next = this.next.bind(this);
+    this.prev = this.prev.bind(this);
+    this.update = this.update.bind(this);
+
+    this.container = document.createElement("div");
+    this.container.classList.add("image-carousel-container");
+    this.container.tabIndex = 0;
+
+    this.content = document.createElement("div");
+    this.content.classList.add("image-carousel-content");
+    this.container.appendChild(this.content);
+
+    this.nextButton = new Button({
+      type: "icon",
+      label: ArrowRight({ width: "44", height: "44" }),
+      onClick: this.next,
+      containerClass: "next",
+    });
+    this.prevButton = new Button({
+      type: "icon",
+      label: ArrowLeft({ width: "44", height: "44" }),
+      onClick: this.prev,
+      containerClass: "prev",
+      disabled: true,
+    });
+    this.container.appendChild(this.nextButton.render());
+    this.container.appendChild(this.prevButton.render());
+
+    this.update({ data, itemWidth, itemHeight });
+
+    if (data && data.length > 0) {
+      this.rebuild();
+    }
+  }
+
+  update({ data, itemWidth, itemHeight }) {
+    if (itemHeight !== undefined) this.state.itemHeight = itemHeight;
+    if (itemWidth !== undefined) this.state.itemWidth = itemWidth;
+    if (data !== undefined) {
+      this.state.data = [...this.state.data, ...data];
+      this.totalWidth = this.state.data.length * this.state.itemWidth;
+    }
+  }
+
+  rebuild() {
+    this.items = [];
+
+    for (let i = 0; i < this.state.data.length; i++) {
+      const item = this.state.data[i];
+      const props = {
+        ...item,
+        index: i,
+        width: this.state.itemWidth,
+        height: this.state.itemHeight,
+      };
+      this.items.push(new DisplayImage(props));
+    }
+  }
+
+  next() {
+    if (this.slideIndex === this.state.data.length) {
+      return;
+    }
+
+    this.slideIndex++;
+    this.animate();
+  }
+
+  prev() {
+    if (this.slideIndex === 1) {
+      return;
+    }
+
+    this.slideIndex--;
+    this.animate();
+  }
+
+  animate() {
+    this.content.style.transform = `translateX(-${
+      (this.slideIndex - 1) * this.state.itemWidth
+    }px)`;
+
+    this.nextButton.update({
+      disabled: this.slideIndex === this.state.data.length,
+    });
+    this.prevButton.update({ disabled: this.slideIndex === 1 });
+  }
+
+  render() {
+    this.container.style.width = `${this.state.itemWidth}px`;
+    this.container.style.height = `${this.state.itemHeight}px`;
+    this.content.replaceChildren();
+    this.content.style.height = `${this.state.itemHeight}px`;
+    this.content.style.width = `${this.totalWidth}px`;
+    Object.values(this.items).forEach((item) => {
+      this.content.appendChild(item.render());
+    });
+    return this.container;
+  }
+}
---
+++ src/components/organisms/image-carousel/image-carousel.stories.js
@@ -0,0 +1,23 @@
+import { ImageCarousel } from "./image-carousel";
+import { data } from "./data.js";
+
+export default {
+  title: "Organisms/ImageCarousel",
+  argTypes: {
+    data: { control: "json" },
+    itemHeight: { control: "number" },
+    itemWidth: { action: "number" },
+  },
+};
+
+const Template = ({ ...args }) => {
+  const slider = new ImageCarousel({ ...args });
+  return slider.render();
+};
+
+export const Simple = Template.bind({});
+Simple.args = {
+  data,
+  itemWidth: 300,
+  itemHeight: 200,
+};
