19885dc92655273b515a436668861f2fb5b44ef1
Thorsten Kober <tkober@gmail.com>
2022-11-25 08:58:39 -0800

    Update product-item.js

--- src/components/organisms/product-list/product-item.js
+++ src/components/organisms/product-list/product-item.js
@@ -18,8 +18,20 @@ export class ProductItem {
 
   render() {
     if (!this.state.data.stocked) this.item.classList.add("out-of-stock");
-    this.item.textContent = this.state.data.name;
+
+    // this.item.textContent = this.state.data.name;
     this.item.role = this.state.role;
+
+    const name = document.createElement("div");
+    name.classList.add("item-name");
+    name.textContent = this.state.data.name;
+    this.item.appendChild(name);
+
+    const price = document.createElement("div");
+    price.classList.add("item-price");
+    price.textContent = this.state.data.price;
+    this.item.appendChild(price);
+    
     return this.item;
   }
 }
