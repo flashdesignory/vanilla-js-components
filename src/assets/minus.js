dca8f3d59afb78e95e1428f78f1a6d040e008b05
Thorsten Kober <tkober@gmail.com>
2022-12-09 15:21:45 -0800

    Add files via upload

---
+++ src/assets/minus.js
@@ -0,0 +1,9 @@
+export const MinusIcon = ({ width = "24", height = "24" } = {}) =>
+  `<svg
+    width="${width}" 
+    height="${height}" 
+    viewBox="0 0 24 24"
+  >
+    <title>minus icon</title>
+    <path d="M0 9h24v6h-24z" />
+  </svg>`;
---
+++ src/assets/plus.js
@@ -0,0 +1,9 @@
+export const PlusIcon = ({ width = "24", height = "24" } = {}) =>
+  `<svg
+    width="${width}" 
+    height="${height}" 
+    viewBox="0 0 24 24"
+  >
+    <title>plus icon</title>
+    <path d="M24 9h-9v-9h-6v9h-9v6h9v9h6v-9h9z" />
+  </svg>`;
